1f62954327dc8267d6fe927198f2cb425ae78b08
Daniel Hyres <danhyres@gmail.com>
2023-01-23 12:17:03 -0500

    Added components and styling

---
+++ App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Die from "./components/Die"
+
+
+export default function App() {
+  
+
+
+
+  return (
+    <main>
+      <div className="dice-container">
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      <Die
+        value="1"
+      />
+      </div>
+    </main>
+  )
+}
