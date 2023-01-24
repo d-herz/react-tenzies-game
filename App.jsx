60b672e0531e3066c32c7d51cd59ff99bf6f1a79
Daniel Hyres <danhyres@gmail.com>
2023-01-24 18:20:33 -0500

    Added holdDice() toggle function, updated rollDice() function, added generateNewDie() helper function. Added additional styling

--- App.jsx
+++ App.jsx
@@ -6,35 +6,63 @@ import {nanoid} from "nanoid"
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice())
 
+  //helper function added to DRY code after rollDice() had to utilize the same code from allNewDice()
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+
+  //Creates a 10 element integer array randomly populated with numbers 1-6 
   function allNewDice() {
     let diceArr = []
     for (let i = 0; i <10; i++) {
-      diceArr.push(
-        {
-          value: Math.ceil(Math.random() * 6),
-          isHeld: false,
-          id: nanoid()
-        }
-      )
+      diceArr.push(generateNewDie())
     }
-    console.log(diceArr)
     return diceArr
   }
 
+  //CB for button press event: calls setState(), checks old value (of state dice array) so we can see if any are being "held", if held, keep that die, otherwise return a new die.  Added helper function "generateNewDie()" so we did not have to repeat the same object definition
   function rollDice() {
-    setDice(allNewDice())
+    // setDice(allNewDice())
+    setDice(prevDice => {
+      return prevDice.map(die => {
+        return die.isHeld ?
+          die :
+          generateNewDie()
+      })
+    })
   }
 
+  //function to pass down to each die element, passing in the parameter as ID. This allows us to avoide "derived state"
+  function holdDice(id) {
+    setDice(prevDice => {
+      return prevDice.map((die) => {
+        return die.id === id ?
+          { ...die, isHeld: !die.isHeld } :
+          die
+      })
+    })
+  }
+
+  //Mapping the "dice" array (from state) and returning a component for each element with the proper props
   const diceElement = dice.map(( num, ind) => {
     return <Die
       key={num.id}
+      id={num.id}
       value={num.value}
       isHeld={num.isHeld}
+      toggleHold={holdDice}
     />
   })
   
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElement}
       </div>
@@ -42,7 +70,7 @@ export default function App() {
         className='roll--dice'
         onClick={rollDice}
       >
-        Roll Dice
+        Roll
       </button>
     </main>
   )
